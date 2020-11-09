31e4a4fb20d027f81fd13d7267a889d79e2db5fe
cfor0 <cfor0@outlook.com>
2020-11-09 14:10:03 -0500

    finished medium exercise

---
+++ medium/script.js
@@ -0,0 +1,9 @@
+
+
+
+const cutPizzaSlices = a => `Each person gets ${8/a} slices of pizza`;
+
+// let sharePizza = cutPizzaSlices(3);
+
+console.log(cutPizzaSlices(2))
+console.log(cutPizzaSlices(3))
