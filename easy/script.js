f4e670a9ea32bc97c70b0898cd3c043d9227913d
cfor0 <cfor0@outlook.com>
2020-11-09 11:50:21 -0500

    finished easy exercise

---
+++ easy/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+const exercise = a => {
+    return `Today's activity is ${a}.`
+}
+
+let run = exercise("running")
+let swim = exercise("swimming")
+
+
+
+console.log(run);
+console.log(swim);
